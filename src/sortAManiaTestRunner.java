cd81e7e4c6081bae529d732570252e9ac1b6abf8
idiallo0123 <idiallo0123@bths.edu>
2019-01-30 08:36:34 -0500

    gekyume

--- src/sortAManiaTestRunner.java
+++ src/sortAManiaTestRunner.java
@@ -6,10 +6,10 @@ public class sortAManiaTestRunner
         SortCompetition team1 = new Team1SortCompetition(); //create an instance of your class
 
         int[] randIntArr = SortingUtil.randomIntsArr(10000); // initialize a random int array
-        String[] randStringArr = randomStringArr(10000, 5); //init a random string array
+        String[] randStringArr = SortingUtil.randomStringArr(10000, 5); //init a random string array
 
         System.out.println("Unsorted");
-        printArr(randIntArr); //print out the unsorted array
+        SortingUtil.printArr(randIntArr); //print out the unsorted array
 
         long time = System.currentTimeMillis(); //start the timer
         int median = team1.challengeOne(ranIntArr); //run your challenge one code
@@ -18,6 +18,6 @@ public class sortAManiaTestRunner
         System.out.println("Median equals: " + median); //print the median you found
 
         System.out.println("Sorted");
-        printArr(randIntArr); //check if array was sorted correctly
+        SortingUtil.printArr(randIntArr); //check if array was sorted correctly
     }
 }
