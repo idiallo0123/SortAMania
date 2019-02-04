7e0acdefbf6b4e90f344056de66375605841509f
idiallo0123 <idiallo0123@bths.edu>
2019-02-04 08:24:44 -0500

    fgxb zsd

--- src/sortAManiaTestRunner.java
+++ src/sortAManiaTestRunner.java
@@ -8,6 +8,9 @@ public class sortAManiaTestRunner
         int[] randIntArr = SortingUtil.randomIntsArr(10000); // initialize a random int array
         String[] randStringArr = SortingUtil.randomStringArr(10000, 5); //init a random string array
 
+        /*challengeOne: Standard sort and process
+         Data Set - an array of 10,000 random integers between 0-10000
+        Task: Sort the list and return the median*/
         System.out.println("----------CHALLENGE 1----------");
         System.out.print("Unsorted: ");
         SortingUtil.printIntArr(randIntArr); //print out the unsorted array
@@ -23,9 +26,29 @@ public class sortAManiaTestRunner
         randIntArr = SortingUtil.randomIntsArr(10000); // initialize a random int array
         randStringArr = SortingUtil.randomStringArr(10000, 5); //init a random string array
 
+        /*challengeTwo: String sorting and searching
+        Data Set - an array of 10,000 random strings (strings will be of length 5)
+        Task: Sort the list and determine if it contains a given string, return the index of the first instance
+        of that string, or -1 if not found*/
         System.out.println("----------CHALLENGE 2----------");
         System.out.print("Unsorted: ");
         SortingUtil.printIntArr(randIntArr); //print out the unsorted array
+
+        /*challengeThree: Mostly Sorted Big Array
+        Data Set - a mostly sorted array of 100,000 integers (>75% of elements are in the correct order)
+        Task: Sort the list and return the median*/
+
+        /*challengeFour: Multi-Dimensional sorting
+        Data Set - a multi-dimensional array int[1000][1000] all elements are random integers between 0-10000
+        Task: Sort each sub-array and then sort the arrays by their median value
+        Return the median of the median array*/
+
+        /*challengeFive: Mystery Sort and Search
+        Data Set - an array of 10,000 objects that implement the comparable interface
+        Task: Sort the array by the compareTo method, and determine if it contains the element given. Return the
+        position of the object, or -1 if not found.
+        Hint: You must use a stable sort for this challenge, equivalent objects should stay in the same order.
+        */
 
     }
 }
