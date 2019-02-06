bd5ff1d082f7ae9cf4461dec47a545122f2baf94
BT_1N3_01 <rmcgovern8516@bths.edu>
2019-02-06 08:37:48 -0500

    Adkbasjkld

--- src/sortAManiaTestRunner.java
+++ src/sortAManiaTestRunner.java
@@ -5,7 +5,8 @@ public class sortAManiaTestRunner
     {
         Team1SortCompetition team1 = new Team1SortCompetition(); //create an instance of your class
 
-        int[] randIntArr = SortingUtil.randomIntsArr(10000); // initialize a random int array
+        int[] randIntArr = SortingUtil.randomIntsArr(10000); // initialize a random int array of 10,000 integers
+        int[] randIntArr2 = SortingUtil.randomIntsArr(100000); //initialize a second random int array of 100,000 integers
         String[] randStringArr = SortingUtil.randomStringArr(10000, 5); //init a random string array
 
         /*challengeOne: Standard sort and process
@@ -40,6 +41,17 @@ public class sortAManiaTestRunner
         //System.out.println("----------CHALLENGE 3----------");
         //System.out.print("Unsorted: ");
         //SortingUtil.printIntArr(randIntArr); //print out the unsorted array
+
+        System.out.print("Unsorted: ");
+        SortingUtil.printIntArr(randIntArr2); //print out the unsorted array
+        long time1 = System.currentTimeMillis(); //start the timer
+        int median1 = team1.challengeOne(randIntArr2); //run your challenge one code
+        time1 = System.currentTimeMillis() - time1; //stop the time
+        System.out.println("Challenge One Time Taken: " + time1 * 0.001 + " Seconds"); ///print the time
+        System.out.println("Median equals: " + median1); //print the median you found
+
+        System.out.println("Sorted: ");
+        SortingUtil.printIntArr(randIntArr2); //check if array was sorted correctly
 
         /*challengeFour: Multi-Dimensional sorting
         Data Set - a multi-dimensional array int[1000][1000] all elements are random integers between 0-10000
