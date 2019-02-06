9d94c7e67118d66546fdd2b9212d34b9872b7978
chuang5877 <chuang5877@bths.edu>
2019-02-06 08:36:55 -0500

    2

--- src/Team1SortCompetition.java
+++ src/Team1SortCompetition.java
@@ -2,9 +2,20 @@ public class Team1SortCompetition
 {
 
     public int challengeOne(int[]arr) {
-    QuickSort quickSort=new QuickSort();
-    quickSort.sort(arr,0,arr.length-1);
-    return arr[arr.length/2];
+        QuickSort quickSort=new QuickSort();
+        quickSort.sort(arr,0,arr.length-1);
+        return arr[arr.length/2];
+    }
+
+    public int challengeTwo(String[] arr,String query){
+        QuickString quickString=new QuickString();
+        quickString.sort(arr,0,arr.length-1);
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i].equals(query)){
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
