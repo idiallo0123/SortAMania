428b59468679199788259d5e0c3fc8be0ef0415e
idiallo0123 <idiallo0123@bths.edu>
2019-02-06 08:39:36 -0500

    xfgdD

--- src/sortAManiaTestRunner.java
+++ src/sortAManiaTestRunner.java
@@ -45,6 +45,18 @@ public class sortAManiaTestRunner
         Data Set - a multi-dimensional array int[1000][1000] all elements are random integers between 0-10000
         Task: Sort each sub-array and then sort the arrays by their median value
         Return the median of the median array*/
+        System.out.println("----------CHALLENGE 4----------");
+        System.out.print("Unsorted: ");
+        SortingUtil.printIntArr(randIntArr); //print out the unsorted array
+        time = System.currentTimeMillis(); //start the timer
+        median = team1.challengeOne(randIntArr); //run your challenge one code
+        time = System.currentTimeMillis() - time; //stop the time
+        System.out.println("Challenge One Time Taken: " + time * 0.001 + " Seconds"); ///print the time
+        System.out.println("Median equals: " + median); //print the median you found
+
+        int [][]CH4Array;
+        CH4Array = new int[1000][1000]
+
 
         /*challengeFive: Mystery Sort and Search
         Data Set - an array of 10,000 objects that implement the comparable interface
